4598d8fbdc2034da03ca653573108199c30f1ad4
alex-greff <alex.j.greff@gmail.com>
2019-06-16 12:45:16 -0400

    Fixed a crash where the router reference wasn't being setup properly

--- server/api/routes/search.js
+++ server/api/routes/search.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.router();
+const router = express.Router();
 const permit = require("../middleware/permission");
 
 const SearchController = require("../controllers/search");
