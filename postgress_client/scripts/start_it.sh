b11c71e82e6bf9efed23bc845f91756752414918
alex-greff <alex.j.greff@gmail.com>
2019-06-28 22:48:04 -0400

    Installed dos2unix command on container and ran /app through it

--- postgress_client/scripts/start_it.sh
+++ postgress_client/scripts/start_it.sh
@@ -6,11 +6,16 @@ SCRIPT_DIR="$(dirname "$0")"
 # Include the common env script
 source "$SCRIPT_DIR/common_env.sh"
 
-
 # Start command
 read -d '' START_CMD << EOF
-    $SET_CONTAINER_ENV;
-    bash
+    # Set environement variables in container
+    $SET_CONTAINER_ENV; 
+
+    # Convert all the files in app to Unix-style endings (hide the output of the command)
+    find /app -type f -print0 | xargs -0 dos2unix &>/dev/null; 
+
+    # Run interactive bash shell
+    bash;
 EOF
 
 # Run the container
