ddfd1ef296a7dcad8ffbe6b033bfbe6cb6114004
alex-greff <alex.j.greff@gmail.com>
2019-06-30 23:05:56 -0400

    Removed the old manual CORS setup

--- server/app.js
+++ server/app.js
@@ -42,20 +42,6 @@ app.options(cors(corsOptions));
 // Enable CORS
 app.use(cors(corsOptions));
 
-// Setup CORS headers manually
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header(
-//         "Access-Control-Allow-Headers",
-//         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//     );
-//     if (req.method === "OPTIONS") { // Preflight request
-//         res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-//         return res.status(200).json({});
-//     }
-//     next();
-// });
-
 // Setup graphql
 app.use('/graphql', graphqlHttp({
     schema: AppModule.schema,
