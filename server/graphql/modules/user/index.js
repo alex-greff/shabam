6761d96195cadcfcf244e7c7956747e694e927f2
alex-greff <alex.j.greff@gmail.com>
2019-07-01 13:36:20 -0400

    Removed some unneeded code

--- server/graphql/modules/user/index.js
+++ server/graphql/modules/user/index.js
@@ -2,10 +2,6 @@ const GraphQLModule = require("@graphql-modules/core").GraphQLModule;
 const typeDefs = require("./user.typeDefs");
 const resolvers = require("./user.resolvers");
 
-const authenticationContext = (session, currentContext, moduleSessionInfo) => {
-    console.log("req", session.headers);
-};
-
 module.exports = new GraphQLModule({
     typeDefs,
     resolvers,
