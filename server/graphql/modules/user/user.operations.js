a6bf067e63a9e81b197f97534cf3d6f6a36f116b
alex-greff <alex.j.greff@gmail.com>
2019-07-01 18:03:53 -0400

    Implemented login resolver

--- server/graphql/modules/user/user.operations.js
+++ server/graphql/modules/user/user.operations.js
@@ -2,31 +2,54 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Utilities = require("../../utilities");
 const db = require("../../../db");
+const KEYS = require("../../../keys");
 
 const DEFAULT_ROLE = "default";
 
 module.exports = {
     login: async (root, { credentials }, context) => {
-        // const { email, password } = credentials;
+        const { email, password } = credentials;
 
-        // let sPasswordHashed;
+        const getUserQuery = await db.query("SELECT user_account_id, email, role, password FROM user_account AS uc WHERE uc.email = %L", email);
+        const bUserFound = getUserQuery.rowCount > 0;
 
-        // try {
-        //     const queryRes = await db.query(`SELECT email, password FROM user_account WHERE email = '${email}'`);
-        //     console.log("Query res", queryRes);
-        // } catch(err) {} // Do nothing
+        if (!bUserFound) {
+            throw new Error("Authorization failed");
+        }
 
-        // TODO: complete
+        const sPasswordHash = getUserQuery.rows[0].password;
 
+        try {
+            // Compare the user's password
+            bcrypt.compareSync(password, sPasswordHash);
+        } catch(err) {
+            // Password comparison failed
+            throw new Error("Authorization failed");
+        }
+
+        const { user_account_id: _id, role } = getUserQuery.rows[0];
+
+        // Generate token
+        const oJWTPayload = {
+            email: email,
+            userID: _id,
+            role: role
+        };
+        const oJWTOptions = {
+            expiresIn: KEYS.jwtExpireTime
+        };
+        const sToken = jwt.sign(oJWTPayload, KEYS.jwtSecret, oJWTOptions);
+
+        // Return the token
         return {
-            token: "r23wrfes32rtw3esrw3ef32wrw3rt3wtw3rw"
+            token: sToken
         }
     },
     signup: async (root, { credentials }, context) => {
         const { email, password } = credentials;
 
         // Attempt to find an already existing user
-        const checkUserQuery = await db.query(`SELECT email FROM user_account WHERE email = %L`, email);
+        const checkUserQuery = await db.query("SELECT email FROM user_account WHERE email = %L", email);
         const bUserExists = checkUserQuery.rowCount > 0;
 
         if (bUserExists) {
