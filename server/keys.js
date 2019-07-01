94c848db57fd53fcf7f4e591d55a6334f54bdd16
alex-greff <alex.j.greff@gmail.com>
2019-07-01 18:03:38 -0400

    Added in JWT secret and expire time keys

--- server/keys.js
+++ server/keys.js
@@ -4,5 +4,7 @@ module.exports = {
     pgUser: process.env.PG_USER,
     pgPort: process.env.PG_PORT,
     pgPassword: process.env.PG_PASSWORD,
+    jwtSecret: process.env.JWT_SECRET,
+    jwtExpireTime: process.env.JWT_EXPIRE_TIME,
     PRODUCTION: process.env.NODE_ENV === "production"
 };
