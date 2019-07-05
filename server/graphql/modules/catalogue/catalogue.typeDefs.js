361a8a384bc8accde12fe186c99b40b8bfbb76ba
alex-greff <alex.j.greff@gmail.com>
2019-07-04 21:24:49 -0400

    Documented catalogue typedef

--- server/graphql/modules/catalogue/catalogue.typeDefs.js
+++ server/graphql/modules/catalogue/catalogue.typeDefs.js
@@ -1,6 +1,7 @@
 const gql = require("graphql-tag");
 
 module.exports = gql`
+    """Meta-data for a track."""
     type TrackMetaData {
         title: String!
         artists: [String!]!
@@ -10,12 +11,14 @@ module.exports = gql`
         updatedDate: String!
     }
 
+    """A track object."""
     type Track {
         _id: ID!
         fingerprintID: Int!
         metaData: TrackMetaData!
     }
 
+    """Input data for adding a new track."""
     input TrackAddInputData {
         title: String!
         artists: [String!]!
@@ -23,6 +26,7 @@ module.exports = gql`
         releaseDate: String
     }
 
+    """Input data for editing a track."""
     input TrackEditInputData {
         title: String
         artists: [String]
@@ -31,13 +35,18 @@ module.exports = gql`
     }
 
     type Query {
+        """Get all tracks in the catalogue."""
         getAllTracks: [Track!]!
+        """Get a specific track in the catalogue."""
         getTrack(trackID: String!): Track!
     }
 
     type Mutation {
+        """Add a new track to the catalogue."""
         addTrack(trackData: TrackAddInputData): Track!
+        """Edit an existing track in the catalogue."""
         editTrack(trackData: TrackEditInputData): Track!
+        """Remove a track from the catalogue."""
         deleteTrack(trackID: Int!): Boolean
     }
 `;
