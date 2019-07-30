06c64ed7da985bbced0ee1b3b2b37b4b54c98204
alex-greff <alex.j.greff@gmail.com>
2019-07-30 15:05:20 -0400

    Updated comment description on WASMLoader module

--- client/src/loaders/WASMLoader.js
+++ client/src/loaders/WASMLoader.js
@@ -1,4 +1,6 @@
-// Takes a wasm module and extracts out its exports
+// Used for extracting and loading WASM modules
+// NOTE: if the WASM module is not component-specific,
+// it is recommended to inject it globally with VueWasm in main.js
 export default async(wasmModule) => {
     const { instance } = await wasmModule();
     return instance.exports;
