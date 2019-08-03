aa219c259c8df7b9ef7c54ea8f3edd356d1423bb
alex-greff <alex.j.greff@gmail.com>
2019-08-02 23:30:22 -0400

    Setup diagnostic recorder view

--- client/src/router.js
+++ client/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Home from '@/views/Home.vue'
+import DiagnosticRecorder from '@/views/DiagnosticRecorder.vue';
 
 Vue.use(Router)
 
@@ -13,6 +14,11 @@ export default new Router({
             path: '/',
             name: 'home',
             component: Home
+        },
+        {
+            path: '/diagnostic-recorder',
+            name: 'diagnosticRecorder',
+            component: DiagnosticRecorder
         },
         // {
         //     path: '/about',
---
+++ client/src/views/DiagnosticRecorder.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Diagnostic Recorder
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
