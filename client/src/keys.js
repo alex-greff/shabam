a7b7169f9db8471b9c08292f346f6ab4d86afa48
alex-greff <alex.j.greff@gmail.com>
2019-08-03 14:50:42 -0400

    Removed plotly api env variables since it's actually not needed

--- client/src/keys.js
+++ client/src/keys.js
@@ -1,5 +1,3 @@
 export default {
-    PLOTLY_API_USERNAME: process.env.VUE_APP_PLOTLY_API_USERNAME,
-    PLOTLY_API_KEY: process.env.VUE_APP_PLOTLY_API_KEY,
     PRODUCTION: process.env.NODE_ENV === "production"   
 }
