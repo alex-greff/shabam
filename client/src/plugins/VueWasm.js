0c04f21945f01edfc69d4da3a99dfa07b68bb976
Alexander Greff <34700852+alex-greff@users.noreply.github.com>
2019-08-06 09:56:13 -0400

    Renamed some parameters

--- client/src/plugins/VueWasm.js
+++ client/src/plugins/VueWasm.js
@@ -5,8 +5,8 @@ const install = async (Vue, options = {}) => {
     Vue.prototype.$wasm = {};
 
     await Promise.all(
-        Object.entries(options.modules).map(async ([moduleName, { js: jsLinkageModule, wasm: wasmModule, exports: exportsList }]) => {
-            const loadedModule = await WASMLoader(jsLinkageModule, wasmModule, exportsList);
+        Object.entries(options.modules).map(async ([moduleName, { js: jsLinkage, wasm: wasmModule, exports: exportMappings }]) => {
+            const loadedModule = await WASMLoader(jsLinkage, wasmModule, exportMappings);
             Vue.prototype.$wasm[moduleName] = loadedModule
         })
     );
