e63c6589ab7d612d9fc11a5eefbcce7e45a36e61
alex-greff <alex.j.greff@gmail.com>
2019-08-12 22:11:17 -0400

    First attempt at getting KFR archive files linked to code compilation

--- client/build.sh
+++ client/build.sh
@@ -1,6 +1,36 @@
 set -e # -e exit immediately if non-zero exit code occurs 
 
+# -------------
+# --- Setup ---
+# -------------
+
+# Setup emsdk environment variables
 source /emsdk/emsdk_env.sh --build=Release;
+
+# ---------------------------------
+# --- Build KFR with emscripten ---
+# ---------------------------------
+
+echo "Building KFR library with emscripten..."
+
+cd /kfr
+
+rm -rf ./build_emscripten
+mkdir ./build_emscripten
+cd ./build_emscripten
+
+emconfigure cmake -DENABLE_TESTS=OFF -DCMAKE_CXX_COMPILER=clang++-4.0 -DCMAKE_BUILD_TYPE=Release ..
+
+emmake make -j
+
+echo "KFR compiled!"
+
+
+# ----------------------
+# --- Build app code ---
+# ----------------------
+
+echo "Building app code..."
 
 # Ensure the needed directories for the build step exist
 mkdir -p /app/wasm /app/build_temp
@@ -12,3 +42,5 @@ make
 
 # Clean the compilation
 make clean
+
+echo "App code compiled!"
