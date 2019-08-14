90af8387dfbc59bba4cf27fbba2cf91682f195ad
alex-greff <alex.j.greff@gmail.com>
2019-08-14 14:05:09 -0400

    Updated downsample function to use async/await

--- client/src/audio/downsample.js
+++ client/src/audio/downsample.js
@@ -5,37 +5,33 @@
  * @param {AudioBuffer} source The source audio buffer. 
  * @param {Number} targetSampleRate The targetted sample rate.
  */
-function downsample(sourceAudioBuffer, targetSampleRate = 16000) {
-    return new Promise((resolve) => {
-        const offlineCtx = new OfflineAudioContext(
-            sourceAudioBuffer.numberOfChannels, 
-            sourceAudioBuffer.duration * sourceAudioBuffer.numberOfChannels * targetSampleRate, 
-            targetSampleRate
-        );
+async function downsample(sourceAudioBuffer, targetSampleRate = 16000) {
+    const offlineCtx = new OfflineAudioContext(
+        sourceAudioBuffer.numberOfChannels, 
+        sourceAudioBuffer.duration * sourceAudioBuffer.numberOfChannels * targetSampleRate, 
+        targetSampleRate
+    );
+
+    const offlineAudioBuffer = offlineCtx.createBuffer(
+        sourceAudioBuffer.numberOfChannels, 
+        sourceAudioBuffer.length, 
+        sourceAudioBuffer.sampleRate
+    );
+
+    // Copy the source data into the offline target AudioBuffer
+    for (let channel = 0; channel < sourceAudioBuffer.numberOfChannels; channel++) {
+        offlineAudioBuffer.copyToChannel(sourceAudioBuffer.getChannelData(channel), channel);
+    }
+
+    // Render the resampled signal
+    const targetSource = offlineCtx.createBufferSource();
+    targetSource.buffer = sourceAudioBuffer;
+    targetSource.connect(offlineCtx.destination);
+    targetSource.start(0);
     
-        const offlineAudioBuffer = offlineCtx.createBuffer(
-            sourceAudioBuffer.numberOfChannels, 
-            sourceAudioBuffer.length, 
-            sourceAudioBuffer.sampleRate
-        );
-    
-        // Copy the source data into the offline target AudioBuffer
-        for (let channel = 0; channel < sourceAudioBuffer.numberOfChannels; channel++) {
-            offlineAudioBuffer.copyToChannel(sourceAudioBuffer.getChannelData(channel), channel);
-        }
-    
-        // Render the resampled signal
-        const targetSource = offlineCtx.createBufferSource();
-        targetSource.buffer = sourceAudioBuffer;
-        targetSource.connect(offlineCtx.destination);
-        targetSource.start(0);
-        offlineCtx.oncomplete = (e) => {
-            const resampledAudioBufferer = e.renderedBuffer;
-            
-            resolve(resampledAudioBufferer);
-        };
-        offlineCtx.startRendering();
-    });
+    const resampledAudioBuffer = await offlineCtx.startRendering();
+
+    return resampledAudioBuffer;
 }
 
 export default downsample;
