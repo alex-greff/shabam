0523c4e7da75caf2069e87917ff7066b7e639e31
alex-greff <alex.j.greff@gmail.com>
2019-09-08 18:11:28 -0400

    Roughed in schema for address databases

--- postgres_client/app/scripts/db/address/initialize_schema.sh
+++ postgres_client/app/scripts/db/address/initialize_schema.sh
@@ -5,5 +5,19 @@ SCRIPTS_DIR="/app/scripts"
 # Include the run_query function
 source "$SCRIPTS_DIR/functions/run_query.sh"
 
+# Start command
+read -d '' SCHEMA_INIT_QUERY << EOF
+    -- address table
+    DROP TABLE IF EXISTS address CASCADE;
+    CREATE TABLE address(
+        address_id SERIAL PRIMARY KEY,
+        address_val INTEGER NOT NULL,
+        couple_val BIGINT NOT NULL
+    );
+EOF
+
+# TODO: uncomment
+# run_query "$SCHEMA_INIT_QUERY"
+
 # TODO: complete
 echo "TODO: complete address database schema initialization"
