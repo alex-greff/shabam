ab9b4ab930857f045b22113cf72c6c29d0dc5400
alex-greff <alex.j.greff@gmail.com>
2019-09-11 23:30:22 -0400

    Added function to get address db keys

--- server/keys.js
+++ server/keys.js
@@ -6,5 +6,12 @@ module.exports = {
     PG_MAIN_PASSWORD: process.env.PG_MAIN_PASSWORD,
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRE_TIME: process.env.JWT_EXPIRE_TIME,
-    PRODUCTION: process.env.NODE_ENV === "production"
+    PRODUCTION: process.env.NODE_ENV === "production",
+    getAddressDbKeys: (addressNum) => ({
+        PG_ADDRESS_HOST: process.env[`PG_ADR${addressNum}_HOST`],
+        PG_ADDRESS_DATABASE: process.env[`PG_ADR${addressNum}_DATABASE`],
+        PG_ADDRESS_USER: process.env[`PG_ADR${addressNum}_USER`],
+        PG_ADDRESS_PORT: process.env[`PG_ADR${addressNum}_PORT`],
+        PG_ADDRESS_PASSWORD: process.env[`PG_ADR${addressNum}_PASSWORD`],
+    })
 };
