eb7e7d286c60f9a2470c9c698acd2560563b5a74
alex-greff <alex.j.greff@gmail.com>
2020-04-18 17:35:12 -0400

    Changed spacing to 4 spaces

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue';
+import { Component, Vue } from 'vue-property-decorator';
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+@Component({
+    components: {
+        HelloWorld
+    }
+})
+export default class Home extends Vue {
+    name = "Home";
 }
 </script>
--- client/vue.config.js
+++ client/vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  lintOnSave: false
+    devServer: {
+        // These flags allow for the dev server to accept the requests routed through the reverse proxy
+        disableHostCheck: true,
+        watchOptions: {
+            poll: true
+        },
+        progress: false
+    },
+    lintOnSave: false
 }
