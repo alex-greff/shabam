4c453b5789375699484d81397fdfe38c717cdff8
alex-greff <alex.j.greff@gmail.com>
2020-04-21 16:37:38 -0400

    Added global and main styles. Added Themer to the project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import './App.scss';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+// Import global scss
+import "./styling/global.scss";
+import "./styling/main.scss";
+
 import NavBar from "@/components/NavBar/NavBar";
 import AppSwitch from "@/router/AppSwitch";
+
 
 const App = () => {
     return (
