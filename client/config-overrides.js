57acb68355c03f93c9ca01cc0d1563907b4ea5b7
alex-greff <alex.j.greff@gmail.com>
2020-04-22 15:24:07 -0400

    Added global.scss to prependData in the sass loader

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const { addWebpackAlias, useEslintRc } = require("customize-cra");
+const { addWebpackAlias, useEslintRc, adjustStyleLoaders } = require("customize-cra");
 
 
 module.exports = function override(config, env) {
@@ -9,6 +9,17 @@ module.exports = function override(config, env) {
 
     // https://github.com/timarney/react-app-rewired/issues/396
     config = useEslintRc(path.resolve(__dirname, '.eslintrc.js'))(config);
+
+    // https://github.com/facebook/create-react-app/issues/7756
+    // and this
+    // https://github.com/arackaf/customize-cra/blob/master/api.md#adjuststyleloaderscallback
+    config = adjustStyleLoaders(({ use }) => {
+        const [, , , , sass] = use;
+
+        if (sass) {
+            sass.options.prependData = `@import "@/styling/global.scss";`;
+        }
+    })(config);
 
     return config;
 }
