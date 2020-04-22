ce46a388da76a5621b9925930403e26e667ffe46
alex-greff <alex.j.greff@gmail.com>
2020-04-21 23:37:56 -0400

    Added eslint customization file

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,10 +1,14 @@
 const path = require("path");
-const { addWebpackAlias } = require("customize-cra");
+const { addWebpackAlias, useEslintRc } = require("customize-cra");
+
 
 module.exports = function override(config, env) {
     config = addWebpackAlias({
         ["@"]: path.resolve(__dirname, "src")
     })(config);
+
+    // https://github.com/timarney/react-app-rewired/issues/396
+    config = useEslintRc(path.resolve(__dirname, '.eslintrc.js'))(config);
 
     return config;
 }
