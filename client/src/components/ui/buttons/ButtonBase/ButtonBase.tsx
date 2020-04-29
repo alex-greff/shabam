3d690e77f516958396e41fb89fc5774f690cb24d
alex-greff <alex.j.greff@gmail.com>
2020-04-29 01:37:21 -0400

    Added some button UI components

---
+++ client/src/components/ui/buttons/ButtonBase/ButtonBase.tsx
@@ -0,0 +1,65 @@
+import React, { FunctionComponent, MouseEvent } from "react";
+import { BaseProps } from "@/types/baseProps.ts"
+import "./ButtonBase.scss";
+import classnames from "classnames";
+import { Link, LinkProps } from "react-router-dom";
+
+export interface Props extends BaseProps, Partial<Pick<LinkProps, "to">> {
+    href?: string;
+    onClick?: (event: MouseEvent<HTMLButtonElement>) => any;
+    disabled?: boolean;
+};
+
+const ButtonBase: FunctionComponent<Props> = (props) => {
+    const { to, href, className, disabled, onClick, ...rest } = props;
+
+    const classNameComputed = classnames("ButtonBase", className, { "disabled": disabled });
+
+    const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+        if (disabled) {
+            event.preventDefault();
+            return;
+        }
+
+        if (onClick) onClick(event);
+    };
+
+    if (to) {
+        return (
+            <Link 
+                {...rest}
+                to={to}
+                className={classNameComputed}
+            >
+                {props.children}
+            </Link>
+        );
+    }
+
+    if (href) {
+        return (
+            <a
+                {...rest}
+                className={classNameComputed}
+            >
+                {props.children}
+            </a>
+        );
+    }
+
+    return (
+        <button
+            {...rest}
+            className={classNameComputed}
+            onClick={handleClick}
+        >
+            {props.children}
+        </button>
+    );
+};
+
+ButtonBase.defaultProps = {
+    disabled: false
+} as Partial<Props>;
+
+export default ButtonBase;
---
+++ client/src/components/ui/buttons/NormalButton/NormalButton.tsx
@@ -0,0 +1,29 @@
+import React, { FunctionComponent } from "react";
+import { BaseProps } from "@/types/baseProps.ts"
+import "./NormalButton.scss";
+import classnames from "classnames";
+
+import ButtonBase, { Props as ButtonBaseProps } from "@/components/ui/buttons/ButtonBase/ButtonBase";
+
+interface Props extends BaseProps, ButtonBaseProps {
+    outlined?: boolean;
+};
+
+const NormalButton: FunctionComponent<Props> = (props) => {
+    const { outlined, className, ...rest } = props;
+    
+    return (
+        <ButtonBase 
+            {...rest}
+            className={classnames("NormalButton", className, { outlined })}
+        >
+            {props.children}    
+        </ButtonBase>
+    );
+};
+
+NormalButton.defaultProps = {
+    outlined: false
+} as Partial<Props>;
+
+export default NormalButton;
