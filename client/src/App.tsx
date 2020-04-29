ca6d9809c1e287e52cf5bcb473aae8808e25e5a4
alex-greff <alex.j.greff@gmail.com>
2020-04-29 01:38:50 -0400

    Roughed in some code for router view transitions

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component, FunctionComponent } from 'react';
 import './App.scss';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Router, Route, BrowserRouter } from 'react-router-dom';
 import { DEFAULT_NAMESPACE, DEFAULT_THEME } from "@/constants";
+import { createBrowserHistory, History } from "history";
 
 import themes from "@/theme/themes";
 import { themeStore } from "@/store/theme/theme.store";
 import ThemeProvider from "@/components/wrappers/ThemeProvider";
 
 import NavBar from "@/components/nav/NavBar/NavBar";
-import AppSwitch from "@/router/AppSwitch";
+import RouteView from "@/router/RouteView";
+import RouteTransitions from "@/router/RouteTransitions";
 
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { Location } from "history";
+
+interface AppLocationState {
+    transition?: string;
+    duration?: number;
+}
+
+
+const Layout: FunctionComponent = ({ children }) => (
+    <div id="App">
+        <NavBar />
+        {children}
+    </div>
+);
+
+interface FactoryProps {
+    classNames: string;
+    timeout: number;
+}
+
+const childFactoryCreator = (props: FactoryProps) => (child:any) => React.cloneElement(child, props);
+
+const history = createBrowserHistory<AppLocationState>();
+
+interface RouteProps {
+    location: Location<AppLocationState>;
+}
 
 class App extends Component {
 
@@ -28,21 +58,74 @@ class App extends Component {
         this.instantiateNamespaces();
         this.instantiateThemes();
     }
+    
 
     render() {
         return (
-            <Router>
+            // <BrowserRouter>
+            <Router history={history}>
                 <ThemeProvider
                     namespace={DEFAULT_NAMESPACE}
                 >
-                    <div id="App">
-                        <NavBar />
-                        <AppSwitch />
-                    </div>
+                    {/* <Router history={history}> */}
+                    {/* <BrowserRouter>
+                            <div id="App">
+                                <NavBar />
+                                <Route 
+                                    render={({ location }) => (
+                                        <RouteTransitions pageKey={location.key}>
+                                            <RouteView location={location} />
+                                        </RouteTransitions>
+                                    )}
+                                    // path="/"
+                                    // component={Test}
+                                />
+                            </div>
+                    </BrowserRouter> */}
+                    {/* </Router> */}
+                    <Route path="/" render={({ location }) => {
+                        const test = location as Location<AppLocationState>;
+
+                        return (
+                            <Layout>
+                                {/* <RouteTransitions pageKey={location.key}>
+                                    <RouteView location={location} />
+                                </RouteTransitions> */}
+
+                                <TransitionGroup
+                                    childFactory={childFactoryCreator({ 
+                                        classNames: test?.state?.transition || "page", 
+                                        timeout: test?.state?.duration || 300 
+                                    })}
+                                >
+                                    <CSSTransition
+                                        key={location.pathname}
+                                        addEndListener={() => {}}
+                                    >
+                                        <RouteView location={location} />
+                                    </CSSTransition>
+                                </TransitionGroup>
+                            </Layout>
+                        );
+                    }}/>
                 </ThemeProvider>
             </Router>
+            // </BrowserRouter>
         );
     }
 }
 
+// const Main = () => (
+//     <BrowserRouter>
+//         <Route path="/" component={App}/>
+//     </BrowserRouter>
+// );
+
+// const Test = (props: any) => (
+//     <RouteTransitions pageKey={props.location.key}>
+//         <RouteView location={props.location} />
+//     </RouteTransitions>
+// );
+
 export default App;
+// export default Main;
---
+++ client/src/router/RouteView.tsx
@@ -0,0 +1,107 @@
+import React, { Suspense, lazy, FunctionComponent } from "react";
+import { Switch, Route } from "react-router-dom";
+import { CSSTransition } from "react-transition-group";
+import { BaseProps } from "@/types/baseProps";
+import { Location } from "history";
+
+import HomeView from "@/views/Home/Home";
+import SearchView from "@/views/Search/Search";
+// import CatalogView from "@/views/Catalog/Catalog";
+const CatalogView = lazy(() => import(/* webpackChunkName: "catalog" */ "@/views/Catalog/Catalog"));
+const AccountView = lazy(() => import(/* webpackChunkName: "account" */ "@/views/Account/Account"));
+const AccountSearchHistoryView = lazy(() => import(/* webpackChunkName: "account-search-history" */ "@/views/AccountSearchHistory/AccountSearchHistory"));
+const AccountCatalogView = lazy(() => import(/* webpackChunkName: "account-catalog" */ "@/views/AccountCatalog/AccountCatalog"));
+const SigninView = lazy(() => import(/* webpackChunkName: "signin" */ "@/views/Signin/Signin"));
+const SignupView = lazy(() => import(/* webpackChunkName: "signup" */ "@/views/Signup/Signup"));
+const PageNotFoundView = lazy(() => import(/* webpackChunkName: "page-not-found" */ "@/views/PageNotFound/PageNotFound"));
+
+export interface Props extends BaseProps {
+    location: Location;
+};
+
+// const routes = [
+//     { path: "/", name: "Home", Component: HomeView },
+//     { path: "/search", name: "Search", Component: SearchView },
+//     { path: "/catalog", name: "Catalog", Component: CatalogView },
+//     { path: "/account/:id", name: "Account", Component: AccountView },
+//     { path: "/account/:id/search-history", name: "AccountSearchHistory", Component: AccountSearchHistoryView },
+//     { path: "/account/:id/catalog", name: "AccountCatalog", Component: AccountCatalogView },
+//     { path: "/signin", name: "Signin", Component: SigninView },
+//     { path: "/signup", name: "Signup", Component: SignupView },
+//     // { name: "PageNotFound", Component: PageNotFoundView }
+// ];
+
+const RouteView: FunctionComponent<Props> = (props) => {
+    // return (
+    //     <Suspense fallback={<div>Loading...</div>}>
+    //         <div className="Container">
+    //             {routes.map(({path, Component }) => (
+    //                 <Route
+    //                     key={path}
+    //                     exact
+    //                     path={path}
+    //                 >
+    //                     {({ match })=>(
+    //                         <CSSTransition
+    //                             in={match != null}
+    //                             timeout={300}
+    //                             classNames="page"
+    //                             unmountOnExit
+    //                         >
+    //                             <div className="Page">
+    //                                 <Component />
+    //                             </div>
+    //                         </CSSTransition>
+    //                     )}
+    //                 </Route>
+    //             ))}
+    //         </div>
+    //     </Suspense>
+    // );
+
+    const { location } = props;
+
+    return (
+        <div>
+            {/* <Suspense fallback={<div>Loading...</div>}> */}
+            <Suspense fallback={null}>
+                <Switch location={location}>
+                    <Route exact path="/" component={HomeView} />
+                    <Route exact path="/search" component={SearchView} />
+                    <Route exact path="/catalog" component={CatalogView} />
+                    <Route exact path="/account/:id" component={AccountView} />
+                    <Route exact path="/account/:id/search-history" component={AccountSearchHistoryView} />
+                    <Route exact path="/account/:id/catalog" component={AccountCatalogView} />
+                    <Route exact path="/signin" component={SigninView} />
+                    <Route exact path="/signup" component={SignupView} />
+                    <Route component={PageNotFoundView} />
+                </Switch>
+            </Suspense>
+        </div>
+    );
+
+    // TODO: make an actual loading component
+    
+
+    // return (
+    //     <Route
+    //         render={({ location }) => (
+    //             <Suspense fallback={<div>Loading...</div>}>
+    //                 <Switch>
+    //                     <Route exact path="/" component={HomeView} />
+    //                     <Route path="/search" component={SearchView} />
+    //                     <Route path="/catalog" component={CatalogView} />
+    //                     <Route path="/account/:id" component={AccountView} />
+    //                     <Route path="/account/:id/search-history" component={AccountSearchHistoryView} />
+    //                     <Route path="/account/:id/catalog" component={AccountCatalogView} />
+    //                     <Route path="/signin" component={SigninView} />
+    //                     <Route path="/signup" component={SignupView} />
+    //                     <Route component={PageNotFoundView} />
+    //                 </Switch>
+    //             </Suspense>
+    //         )}
+    //     />
+    // );
+};
+
+export default RouteView;
