27e212a4238386ed8a0f247666750f1f0b8d9adc
alex-greff <alex.j.greff@gmail.com>
2020-05-04 21:35:57 -0400

    Moved out graphql API endpoint to an environment variable

--- client/src/utilities/validation.ts
+++ client/src/utilities/validation.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import KEYS from "@/keys";
 
 interface CheckUsernameResponse {
     data: {
@@ -22,7 +23,8 @@ export const checkUsername = async (username: string) => {
         variables
     }
 
-    const result = await axios.post<CheckUsernameResponse>("/api/graphql", data, {
+    // TODO: implement some form of debouncing
+    const result = await axios.post<CheckUsernameResponse>(KEYS.GRAPHQL_API_ENDPOINT, data, {
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
