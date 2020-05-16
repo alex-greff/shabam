0c08f1bd2cccd00efff6c7f1e699f9a102d72bdd
alex-greff <alex.j.greff@gmail.com>
2020-05-16 15:16:23 -0400

    Added some test code and build scripts for wasm. Updated client dockerfile to work with emscripten environment. Updated client npm scripts and upgraded node-sass to work with node 14.x

---
+++ client/src/wasm/src/main.cpp
@@ -0,0 +1,16 @@
+#include <emscripten.h>
+#include<iostream>
+#include "fibonacci.hpp"
+// #include <kfr/dft.hpp>
+
+// This avoids C++'s name mangling
+extern "C" {
+    int test(int n);
+}
+
+
+EMSCRIPTEN_KEEPALIVE
+int test(int n) {
+    std::cout << "Hello World" << std::endl;
+    return fib(n);
+}
