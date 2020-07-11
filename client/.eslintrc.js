ed72bd21905a2ce8302ecfdfdbbc2b1f4790458f
alex-greff <alex.j.greff@gmail.com>
2020-07-10 22:56:16 -0400

    Tweak eslint settings and added default formatter associations for jsx and tsx files

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -2,6 +2,16 @@ module.exports = {
   extends: "react-app",
   rules: {
     "no-throw-literal": 0,
-    "max-len": ["warn", { code: 80 }],
+    "max-len": [
+      "warn",
+      {
+        code: 80,
+        comments: 80, 
+        ignoreRegExpLiterals: true,
+        ignoreTemplateLiterals: true,
+        ignoreStrings: true,
+        ignoreUrls: true
+      },
+    ],
   },
 };
