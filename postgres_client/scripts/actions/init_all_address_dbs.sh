0f2af5b7fb4bf4fc4bab098b1fbe41035fded122
alex-greff <alex.j.greff@gmail.com>
2020-09-04 22:06:42 -0400

    Added address databases and added scripts for initializing them

---
+++ postgres_client/scripts/actions/init_all_address_dbs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Initializes all the address databases
+# Note: will say yes to all the confirmation dialogs
+
+# Parameters:
+# $1: total number of address databases
+# $2: skip warning prompt
+
+SCRIPTS_DIR="$(pwd)/scripts"
+
+# Include the run_container function
+source "$SCRIPTS_DIR/functions/run_container.sh"
+
+# Include the confirmation_prompt function
+source "$SCRIPTS_DIR/functions/confirmation_prompt.sh"
+
+NUM_ADDRESS_DBS="$1"
+SKIP_PROMPT="$2"
+
+WARNING_PROMPT="\e[31mDANGER ZONE:\e[0m running this will reset all tables and data of all address databases. Run this command?"
+ACCEPTANCE_COMMAND="for i in \$(seq 1 "$NUM_ADDRESS_DBS"); do bash ./scripts/actions/init_address_db.sh "\""\$i"\"" 'true'; done"
+REJECTION_COMMAND='echo Cancelled schema initialization'
+
+if [ "$SKIP_PROMPT" = true ]; then
+  eval "$ACCEPTANCE_COMMAND"
+else
+  # Start up the container with the bash running interactively
+  confirmation_prompt "$WARNING_PROMPT" "$ACCEPTANCE_COMMAND" "$REJECTION_COMMAND"
+fi
--- postgres_client/scripts/functions/confirmation_prompt.sh
+++ postgres_client/scripts/functions/confirmation_prompt.sh
@@ -6,6 +6,7 @@
 # $1: Confirmation text
 # $2: Code to run on acceptance
 # $3: Code to run on rejection
+
 confirmation_prompt() {
     read -r -p "$(echo -e $1) [y/N] " response
     if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
