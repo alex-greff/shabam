95027aed0119d56c99a64a4826f30e47cf2f8cec
alex-greff <alex.j.greff@gmail.com>
2021-12-22 22:41:33 -0500

    Added TODO reminder for porting over wasm fingerprint generator

--- fingerprint-lib/src/index.ts
+++ fingerprint-lib/src/index.ts
@@ -1,3 +1,5 @@
 export * from "./types";
 export { generateFingerprint as functionalGenerateFingerprint } from "./generators/functional.fingerprint-generator";
 export { generateFingerprint as iterativeGenerateFingerprint } from "./generators/iterative.fingerprint-generator";
+
+// TODO: eventually port over the WASM fingerprint generator as well
