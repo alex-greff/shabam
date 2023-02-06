f68e123f8e58afc288dbebf9e44ecd3d45849fee
alex-greff <alex.j.greff@gmail.com>
2023-02-05 20:13:07 -0800

    Implemented records table in C++

---
+++ core-lib/src/native/search/records_table.hpp
@@ -0,0 +1,51 @@
+#ifndef SHABAM_CORE_LIB_RECORDS_TABLE
+#define SHABAM_CORE_LIB_RECORDS_TABLE
+
+#include "../fingerprint/fingerprint.hpp"
+
+/** A single record. */
+struct record_t {
+  uint32_t anchor_frequency;
+  uint32_t point_frequency;
+  uint32_t delta;
+  uint32_t anchor_absolute_time;
+};
+
+/**
+ * Computes the records tables from a given fingerprint.
+*/
+class RecordsTable {
+private:
+  Fingerprint &fingerprint;
+
+  /**
+   * The number of points in a target zone.
+  */
+  size_t target_zone_size;
+
+public:
+  RecordsTable(Fingerprint &fingerprint, size_t target_zone_size);
+  ~RecordsTable();
+
+  /**
+   * The array of records from the fingerprint.
+  */
+  record_t *records;
+
+  /**
+   * The number of items in the records array.
+  */
+  size_t num_records;
+
+  /**
+   * Computes the records array.
+  */
+  void Compute();
+
+  /**
+   * Gives the number of target zones.
+  */
+  size_t GetNumTargetZones();
+};
+
+#endif
