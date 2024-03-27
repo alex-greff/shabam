b81809983b311294587a3c9291a85967fe639ecc
Alex Greff <alex.j.greff@gmail.com>
2024-03-26 22:18:42 -0700

    Fixed axes

---
+++ mvp-cli/commands.sh
@@ -0,0 +1,2 @@
+python main.py add audio/self_destruct_clip_1.wav
+python main.py add audio/self_destruct_clip_2.wav
--- mvp-cli/main.py
+++ mvp-cli/main.py
@@ -5,6 +5,7 @@ from scipy import signal
 import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
+from numerize import numerize 
 
 DATA_DIR = "data"
 DEBUG_DIR = "debug"
@@ -17,6 +18,19 @@ app = typer.Typer()
 def _setup_dirs():
   Path(DEBUG_DIR).mkdir(parents=True, exist_ok=True)
   Path(DATA_DIR).mkdir(parents=True, exist_ok=True)
+
+def generate_xaxis_formatter(num_windows: int, duration: float):
+  def xaxis_formatter(x: float, pos):
+    curr_time = round(x/num_windows*duration, 2)
+    return str(curr_time)
+  return xaxis_formatter
+
+def generate_yaxis_formmatter(sample_rate: int, num_bins: int):
+  max_freq = sample_rate / 2 # Nyquist theorem
+  def yaxis_formatter(x: float, pos):
+    curr_freq = int((x / num_bins) * max_freq)
+    return numerize.numerize(curr_freq, 2)
+  return yaxis_formatter
 
 @app.command()
 def add(track_filepath: str):
@@ -52,23 +66,41 @@ def add(track_filepath: str):
   _, _, Sxx = signal.spectrogram(data_mono, sample_rate, nfft=FFT_SIZE)
   Sxx = Sxx[:-1, :]
 
+  num_bins = Sxx.shape[0]
+  num_windows = Sxx.shape[1]
+
+  print(f"Sxx.shape {Sxx.shape}") # TODO: remove
+
   plt.clf()
   # Plot spectrogram of mono signal
+  ax = plt.subplot()
   plt.pcolormesh(Sxx, shading='flat')
   plt.yscale("symlog")
-  plt.ylabel('Frequency Bins')
-  plt.xlabel('Windows')
+  # plt.yscale("linear")
+  plt.ylabel('Frequency (Hz)')
+  plt.xlabel('Time (s)')
+  ax.yaxis.set_major_formatter(generate_yaxis_formmatter(sample_rate, num_bins))
+  ax.xaxis.set_major_formatter(generate_xaxis_formatter(num_windows, duration))
   plt.savefig(f"{DEBUG_DIR}/{track_title}_mono_freqdomain.png")
 
   _, _, Sxx_ds = signal.spectrogram(ds_data, ds_sample_rate, nfft=FFT_SIZE)
   Sxx_ds = Sxx_ds[:-1, :]
 
+  num_bins_ds = Sxx_ds.shape[0]
+  num_windows_ds = Sxx_ds.shape[1]
+
+  print(f"Sxx_ds.shape {Sxx_ds.shape}") # TODO: remove
+
   plt.clf()
   # Plot spectrogram of downsampled signal
+  ax = plt.subplot()
   plt.pcolormesh(Sxx_ds, shading='flat')
   plt.yscale("symlog")
-  plt.ylabel('Frequency Bins')
-  plt.xlabel('Windows')
+  # plt.yscale("linear")
+  plt.ylabel('Frequency (Hz)')
+  plt.xlabel('Time (s)')
+  ax.yaxis.set_major_formatter(generate_yaxis_formmatter(ds_sample_rate, num_bins_ds))
+  ax.xaxis.set_major_formatter(generate_xaxis_formatter(num_windows_ds, duration))
   plt.savefig(f"{DEBUG_DIR}/{track_title}_ds_freqdomain.png")
 
 
