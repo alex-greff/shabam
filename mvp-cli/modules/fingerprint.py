b0bb63f185bef0a08f81c6f2db11401b04f1c701
Alex Greff <alex.j.greff@gmail.com>
2024-09-04 11:19:02 -0700

    Improved doc strings

--- mvp-cli/modules/fingerprint.py
+++ mvp-cli/modules/fingerprint.py
@@ -4,11 +4,14 @@ from math import floor
 # note: num_bins = FFT_SIZE / 2
 def _get_partition_range(a: int, b: int, c: int, x: int) -> Tuple[int, int]:
   """
-  Given params for desired frequency partitioning, and index of desired partition,
-    returns tuple of start and end point of the partition
-  a: number of partitions to divide frequency range into
-  b: number of bins (half of FFT size)
-  c: partition curve tension
+  Given the params for the desired frequency partitioning, and index of desired
+  partition, returns tuple of start and end point of the partition.
+
+  Params:
+    a: number of partitions to divide frequency range into
+    b: number of bins (half of FFT size)
+    c: partition curve tension
+    x: the current partition number (zero-based)
   """
   def f(x):
     return floor((b/(c-1))*(c**(x/a) - 1))
@@ -16,4 +19,13 @@ def _get_partition_range(a: int, b: int, c: int, x: int) -> Tuple[int, int]:
   return (f(x), f(x+1)-1)
 
 def get_partition_ranges(a: int, b: int, c: int) -> List[Tuple[int, int]]:
+  """
+  Given the params for the desired partitioning, returns a list of tuples
+  of the start and end indexes (inclusive for both) for all the partition ranges.
+
+  Params:
+    a: number of partitions to divide frequency range into
+    b: number of bins (half of FFT size)
+    c: partition curve tension
+  """
   return [_get_partition_range(a, b, c, x) for x in range(a)]
