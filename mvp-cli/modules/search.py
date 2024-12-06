bdf4c795105be0e73a762da42acb4a79defff0d6
alex-greff <alex.j.greff@gmail.com>
2024-12-05 22:26:21 -0800

    Fixed small loading bug and added a todo

--- mvp-cli/modules/search.py
+++ mvp-cli/modules/search.py
@@ -10,17 +10,19 @@ RecordsDatabase = records.RecordsTableEncoded
 def _construct_record_table_database() -> RecordsDatabase:
   rtdb: RecordsDatabase = dict()
 
-  computed_rt_files = glob.glob(f"{config.DATA_DIR}/*.rt.pkl")
+  computed_rt_files = glob.glob(f"{config.DATA_DIR}/track/*.rt.pkl")
   print(">>> computed_rts", computed_rt_files)  # TODO: remove
 
   for rt_file in computed_rt_files:
     rt_name = Path(rt_file).stem
 
     rt: Optional[records.RecordsTableEncoded] = cache.load(
-        rt_name, is_numpy=False)
+        rt_name, "track", is_numpy=False)
     assert rt is not None
 
     # Merge loaded record table into the record table database
+    # TODO: we can't merge like this, we need to use lists for multiple entries
+    # with the same address
     rtdb = {**rtdb, **rt}
 
   return rtdb
