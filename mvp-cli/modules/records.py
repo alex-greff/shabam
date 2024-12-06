677cc9d3f138373e8648c4fd3e2cd74d1f1cec4f
alex-greff <alex.j.greff@gmail.com>
2024-12-05 17:38:06 -0800

    Implemented the record encoding and decoding functions and their tests

--- mvp-cli/modules/records.py
+++ mvp-cli/modules/records.py
@@ -1,19 +1,157 @@
+"""Module for handling track records."""
+
 from typing import Tuple
 import numpy as np
 import nptyping as npt
 
 
-def encode_couple(absTime: npt.UInt32, trackId: npt.UInt32) -> npt.UInt64:
-  # Bit layout
-  # |                          64-bit integer                         |
-  # |xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx|xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx|
-  # |            absTime             |             trackId            |
-  return 0
+class AddressInvalidParameterException(Exception):
+  """
+  Raised when an invalid parameter is passed to an address function
+  """
+
+
+def encode_address(
+    anchor_freq: npt.UInt16,
+    point_freq: npt.UInt16,
+    delta: npt.UInt16
+) -> npt.UInt32:
+  """
+  Encodes an address into its unsigned 32-bit integer representation.
+
+  Bit layout:
+  ```
+  |                    32-bit unsigned integer                    |
+  |x x x x x x x x x|x x x x x x x x x|x x x x x x x x x x x x x x|
+  |   anchor freq   |    point freq   |           delta           |
+  ```
+
+  Params:
+    `anchor_freq`: the anchor frequency value (maximum value 511)
+    `point_freq`: the point frequency value (maximum value 511)
+    `delta`: the delta value between the anchor and point frequencies (maximum value 16383)
+  Returns:
+    An unsigned 32-bit integer representation of the address
+  """
+
+  PARTITION_MAX = 512  # 2^9
+  DELTA_MAX = 16384  # 2^14
+
+  # Check that we can actually encode it in a 32-bit integer
+  if anchor_freq >= PARTITION_MAX or point_freq >= PARTITION_MAX or delta >= DELTA_MAX:
+    raise AddressInvalidParameterException("An input value is too big")
+
+  encoded = np.uint32(anchor_freq)  # add anchor_freq
+  encoded = encoded << np.uint32(9)  # make space for point_freq
+  encoded = encoded | np.uint32(point_freq)
+  encoded = encoded << np.uint32(14)  # make space for delta
+  encoded = encoded | np.uint32(delta)
+  return encoded
+
+
+def decode_address(
+    address: npt.UInt32
+) -> Tuple[npt.UInt16, npt.UInt16, npt.UInt16]:
+  """
+  Decodes an unsigned 32-bit integer address representation into its component
+  parts.
+
+  Bit layout:
+
+  ```
+  |                    32-bit unsigned integer                    |
+  |x x x x x x x x x|x x x x x x x x x|x x x x x x x x x x x x x x|
+  |   anchor freq   |    point freq   |           delta           |
+  ```
+
+  Params:
+    `address`: the unsigned 32-bit integer address representation
+  Returns:
+    A tuple of [`anchor_freq`, `point_freq`, `delta`]
+
+    where,
+      `anchor_freq`: the anchor frequency value (maximum value 511)
+      `point_freq`: the point frequency value (maximum value 511)
+      `delta`: the delta value between the anchor and point frequencies (maximum value 16383)
+  """
+
+  # 11111111100000000000000000000000
+  # = 511 << 23
+  # = 4286578688 (unsigned)
+  ANCHOR_MASK = np.uint32(4286578688)
+
+  # 00000000011111111100000000000000
+  # = 511 << 14
+  # = 8372224
+  POINT_MASK = np.uint32(8372224)
+
+  # 00000000000000000011111111111111
+  DELTA_MASK = np.uint32(16383)
+
+  anchor_freq = np.uint16(np.bitwise_and(
+      address, ANCHOR_MASK) >> np.uint32(23))
+  point_freq = np.uint16(np.bitwise_and(address, POINT_MASK) >> np.uint32(14))
+  delta_freq = np.uint16(np.bitwise_and(address, DELTA_MASK))
+
+  return (anchor_freq, point_freq, delta_freq)
+
+
+def encode_couple(abs_time: npt.UInt32, track_id: npt.UInt32) -> npt.UInt64:
+  """
+  Encodes a couple into its unsigned 64-bit integer representation.
+
+  Bit layout:
+  ```
+  |                      64-bit unsigned integer                    |
+  |xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx|xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx|
+  |            absTime             |             trackId            |
+  ```
+
+  Params:
+    `abs_time`: the absolute time position (maximum value `UInt32.max`)
+    `track_id`: the identifier of the associated track (maximum value `UInt32.max`)
+  Returns:
+    An unsigned 64-bit integer representation of the couple
+  """
+
+  encoded = np.uint64(abs_time)  # add abs_time
+  encoded = encoded << np.uint64(32)  # make space for track_id
+  encoded = encoded | np.uint64(track_id)
+  return encoded
 
 
 def decode_couple(couple: npt.UInt64) -> Tuple[npt.UInt32, npt.UInt32]:
-  # Bit layout
-  # |                          64-bit integer                         |
-  # |xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx|xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx|
-  # |            absTime             |             trackId            |
-  pass
+  """
+  Decodes an unsigned 64-bit integer couple representation into its component
+  parts.
+
+  Bit layout:
+  ```
+  |                      64-bit unsigned integer                    |
+  |xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx|xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx|
+  |            absTime             |             trackId            |
+  ```
+
+  Params:
+    `couple`: the unsigned 64-bit integer couple representation
+  Returns:
+    A tuple of [`abs_time`, `track_id`]
+
+    where,
+      `abs_time`: the absolute time position (maximum value `UInt32.max`)
+      `track_id`: the identifier of the associated track (maximum value `UInt32.max`)
+  """
+
+  # 1111111111111111111111111111111100000000000000000000000000000000
+  # 4294967295n << 32n
+  # 18446744069414584320n
+  ABS_TIME_MASK = np.uint64(18446744069414584320)
+
+  # 11111111111111111111111111111111
+  TRACK_ID_MASK = np.uint64(4294967295)
+
+  abs_time = np.uint32(
+      np.bitwise_and(couple, ABS_TIME_MASK) >> np.uint64(32))
+  track_id = np.uint32(np.bitwise_and(couple, TRACK_ID_MASK))
+
+  return (abs_time, track_id)
--- mvp-cli/modules/tests/test_records.py
+++ mvp-cli/modules/tests/test_records.py
@@ -1,7 +1,95 @@
+# pylint: disable=missing-module-docstring, missing-class-docstring, missing-function-docstring
+import numpy as np
+import pytest
 from modules import records
 
 
-class TestEncodeCouple:
+class TestEncodeAndDecodeAddress:
   def test_zeros(self):
-    couple = records.encode_couple(0, 0)
+    address = records.encode_address(np.uint16(0), np.uint16(0), np.uint16(0))
+    assert address == 0
+
+    [anchor_freq, point_freq,
+     delta_freq] = records.decode_address(np.uint32(0))
+    assert anchor_freq == 0
+    assert point_freq == 0
+    assert delta_freq == 0
+
+  def test_typical(self):
+    address = records.encode_address(
+        np.uint16(123), np.uint16(456), np.uint16(789))
+    assert address == 1039270677
+
+    [anchor_freq, point_freq,
+     delta_freq] = records.decode_address(np.uint32(1039270677))
+    assert anchor_freq == 123
+    assert point_freq == 456
+    assert delta_freq == 789
+
+  def test_max(self):
+    address = records.encode_address(
+        np.uint16(511), np.uint16(511), np.uint16(16383))
+    assert address == 4294967295
+
+    [anchor_freq, point_freq,
+     delta_freq] = records.decode_address(np.uint32(4294967295))
+    assert anchor_freq == 511
+    assert point_freq == 511
+    assert delta_freq == 16383
+
+  def test_invalid_params(self):
+    # anchor_freq param too big
+    with pytest.raises(records.AddressInvalidParameterException):
+      records.encode_address(np.uint16(512), np.uint16(0), np.uint16(0))
+
+    # point_freq param too big
+    with pytest.raises(records.AddressInvalidParameterException):
+      records.encode_address(np.uint16(0), np.uint16(512), np.uint16(0))
+
+    # delta param too big
+    with pytest.raises(records.AddressInvalidParameterException):
+      records.encode_address(np.uint16(0), np.uint16(0), np.uint16(16384))
+
+
+class TestEncodeAndDecodeCouple:
+  def test_zeros(self):
+    couple = records.encode_couple(np.uint32(0), np.uint32(0))
     assert couple == 0
+
+    [abs_time, track_id] = records.decode_couple(np.uint64(0))
+    assert abs_time == 0
+    assert track_id == 0
+
+  def test_typical(self):
+    couple = records.encode_couple(np.uint32(123), np.uint32(456))
+    assert couple == 528280977864
+
+    [abs_time, track_id] = records.decode_couple(np.uint64(528280977864))
+    assert abs_time == 123
+    assert track_id == 456
+
+  def test_max(self):
+    uint32_max = np.iinfo(np.uint32).max
+    uint64_max = np.iinfo(np.uint64).max
+
+    couple = records.encode_couple(uint32_max, uint32_max)
+    assert couple == uint64_max
+
+    [abs_time, track_id] = records.decode_couple(uint64_max)
+    assert abs_time == uint32_max
+    assert track_id == uint32_max
+
+  def test_abs_time_overflow(self):
+    uint32_max = np.iinfo(np.uint32).max
+
+    couple = records.encode_couple(uint32_max + 1, np.uint32(0))
+    # the abs_time parameter wraps around to 0
+    assert couple == 0
+
+  def test_track_id_overflow(self):
+    uint32_max = np.iinfo(np.uint32).max
+
+    couple = records.encode_couple(np.uint32(0), uint32_max + 1)
+    # the track_id parameter wraps around to b100000000000000000000000000000000
+    # which is 4294967296
+    assert couple == 4294967296
